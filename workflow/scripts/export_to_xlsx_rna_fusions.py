d1489c1083faf21e92a827294e51410fe0e34491
Arielle R Munters <arielle@reivant.se>
2026-08-28 16:31:50 +0200

    refactor: sorted fusioncatcher short table, add dux4 table

--- workflow/scripts/export_to_xlsx_rna_fusions.py
+++ workflow/scripts/export_to_xlsx_rna_fusions.py
@@ -4,6 +4,7 @@ import xlsxwriter
 import datetime
 import sys
 import logging
+import operator
 
 logging.basicConfig(
     format="{asctime} - {levelname} - {message}",
@@ -29,7 +30,7 @@ def convert_columns_to_letter(nr_columns):
 """ Prepping input data """
 logging.info("Prepping input data")
 
-logging.debug("Loading fusioncatcher genelist if exists")
+logging.info("Loading fusioncatcher genelist if exists")
 single_genes = set()
 gene_pairs = set()
 if snakemake.params.fusioncatcher_genelist:
@@ -48,10 +49,11 @@ if snakemake.params.fusioncatcher_genelist:
             else:
                 single_genes.add(entry.upper())
         logging.debug(f"Loaded {len(single_genes)=} single genes and {len(gene_pairs)=} gene pairs")
+        logging.debug("Genes included in Fusioncatcher short table: " + str(single_genes | gene_pairs))
 
 
+logging.info(f"Loading arriba results: {snakemake.input.arriba=}")
 sample_name = snakemake.input.arriba.split("/")[-1].split("_")[0]
-logging.debug(f"Loading arriba results: {snakemake.input.arriba=}")
 arriba_table = {"headers": [], "data": []}
 with open(snakemake.input.arriba, "r") as arriba_tsv:
     for lline in arriba_tsv:
@@ -61,29 +63,41 @@ with open(snakemake.input.arriba, "r") as arriba_tsv:
             arriba_table["data"].append(lline.strip().split("\t"))
 
 
-logging.debug(f"Loading fusioncatcher results: {snakemake.input.fusioncatcher=}")
+# Loading fusioncatcher results in to three different lists/tables
+#     - fusioncatcher_table: the whole table
+#     - fusioncatcher_table_short: fusioncatcher results with only genes from single_genes and gene_pairs included, inframe fusions in one list and the rest in another to be able to print with in-frame fusions first
+#     - fusioncatcher_dux_table: fusions DUX4::IGH or DUX4::ERG
+logging.info(f"Loading fusioncatcher results: {snakemake.input.fusioncatcher=}")
 fusioncatcher_table = {"headers": [], "data": []}
-fusioncatcher_table_short = {"headers": [], "data": []}
+fusioncatcher_table_short = {"inframe_rows": [], "the_rest_rows": []}
+fusioncatcher_dux_table = {"data": []}
+dux_pairs = [frozenset(["DUX4", "ERG"]), frozenset(["DUX4", "IGH@"])]
+logging.debug(f"{dux_pairs=}")
 with open(snakemake.input.fusioncatcher, "r") as fusioncatcher_tsv:
     first_row = True
     for lline in fusioncatcher_tsv:
         line = lline.strip().split("\t")
         if first_row:
             [fusioncatcher_table["headers"].append({"header": column}) for column in line]
-            [fusioncatcher_table_short["headers"].append({"header": column}) for column in line]
             first_row = False
         else:
             fusioncatcher_table["data"].append(line)
             g1 = line[0].strip().upper()
             g2 = line[1].strip().upper()
-            if g1 in single_genes or g2 in single_genes or frozenset([g1, g2]) in gene_pairs:
-                fusioncatcher_table_short["data"].append(line)
-logging.debug(f"Calculating number of DUX4 hits in short fusioncatcher table. ")
-dux4_calls = sum(1 for row in fusioncatcher_table_short["data"] if "DUX4" in row[0].upper() or "DUX4" in row[1].upper())
-logging.debug(f"Number of DUX4 hits in short fusioncatcher table: {dux4_calls}")
+            predicted_effect = line[fusioncatcher_table["headers"].index("Predicted_effects")].strip().lower()
+            if frozenset([g1, g2]) in dux_pairs:
+                fusioncatcher_dux_table["data"].append(line)
+            elif g1 in single_genes or g2 in single_genes or frozenset([g1, g2]) in gene_pairs:
+                if "in-frame" in predicted_effect:
+                    fusioncatcher_table_short["inframe_rows"].append(line)
+                else:
+                    fusioncatcher_table_short["the_rest_rows"].append(line)
+logging.debug(f"Number of dux4-pairs found: {len(fusioncatcher_dux_table['data'])=}")
+fusioncatcher_table_short["data"] = fusioncatcher_table_short["inframe_rows"] + fusioncatcher_table_short["the_rest_rows"]
+logging.debug(f"Number of fusions in Fusioncatcher short table: {len(fusioncatcher_table_short['data'])=}")
 
 
-logging.debug(f"Loading starfusion input: {snakemake.input.star_fusion=}")
+logging.info(f"Loading starfusion input: {snakemake.input.star_fusion=}")
 starfusion_table = {"headers": [], "data": []}
 with open(snakemake.input.star_fusion, "r") as starfusion_tsv:
     for lline in starfusion_tsv:
@@ -119,28 +133,40 @@ worksheet_overview.write_url(8, 0, "internal:'Arriba'!A1", string="Arriba fusion
 worksheet_overview.write_url(9, 0, "internal:'Fusioncatcher'!A1", string="Fusioncatcher results")
 worksheet_overview.write_url(10, 0, "internal:'StarFusion'!A1", string="StarFusion results")
 
-worksheet_overview.write(12, 0, "DUX4-IGH and DUX4-ERG hits from Fusioncatcher", format_bold)  # keep?
-worksheet_overview.write(13, 0, "Number of hits: " + str(dux4_calls))
+worksheet_overview.write(12, 0, "DUX4-IGH and DUX4-ERG hits from Fusioncatcher", format_bold)
+worksheet_overview.write(13, 0, "Number of hits: " + str(len(fusioncatcher_dux_table["data"])))
 worksheet_overview.write(
-    14, 0, "Genes included in Fusioncatcher short table: " + ", ".join(sorted(list(single_genes | gene_pairs)))
+    15,
+    0,
+    "Genes included in Fusioncatcher short table: "
+    + ", ".join(sorted(list(single_genes) + ["|".join(sorted(pair)) for pair in gene_pairs])),
 )
 
-i = 16
+i = 18
+logging.debug(f"Creating the DUX4 table")
+column_end = convert_columns_to_letter(len(fusioncatcher_table["headers"]))
+if len(fusioncatcher_dux_table["data"]) > 0:
+    table_area_dux = "A" + str(i) + ":" + column_end + str(len(fusioncatcher_dux_table["data"]) + i)
+else:
+    table_area_dux = "A" + str(i) + ":" + column_end + str(i + 1)
+
+worksheet_overview.add_table(
+    table_area_dux,
+    {"columns": fusioncatcher_table["headers"], "data": fusioncatcher_dux_table["data"], "style": "Table Style Light 1"},
+)
+
+
+i = i + len(fusioncatcher_dux_table["data"]) + 3
 logging.debug(f"Creating fusioncatcher short table")
-worksheet_overview.write(i - 2, 0, "Fusioncatcher hits matching gene list", format_bold)
-column_end = convert_columns_to_letter(len(fusioncatcher_table_short["headers"]))
 if len(fusioncatcher_table_short["data"]) > 0:
     table_area_short = "A" + str(i) + ":" + column_end + str(len(fusioncatcher_table_short["data"]) + i)
 else:
     table_area_short = "A" + str(i) + ":" + column_end + str(i + 1)
-    worksheet_overview.add_table(
-        table_area_short,
-        {
-            "columns": fusioncatcher_table_short["headers"],
-            "data": fusioncatcher_table_short["data"],
-            "style": "Table Style Light 1",
-        },
-    )
+
+worksheet_overview.add_table(
+    table_area_short,
+    {"columns": fusioncatcher_table["headers"], "data": fusioncatcher_table_short["data"], "style": "Table Style Light 1"},
+)
 
 # Arriba sheet
 logging.debug(f"Creating Arriba sheet")
