cdc88b88430a0693dca9ceda3bcb5c542e21a624
Arielle R Munters <45485665+elleira@users.noreply.github.com>
2026-08-31 11:28:29 +0200

    refactor: fusioncatcher genelist (#146)
    * refactor: add fusioncatcher genelist table to overviewsheet
    
    * style: add a global config for formatting
    
    * refactor: add fusioncatcher_genelist to configs and export_xlsx_rna_fusions rule
    
    * refactor: sorted fusioncatcher short table, add dux4 table
    
    * refactor: predicted effect from header restructure
    
    * refactor: added DUX4 genepairs to genelist, if more dux4-pairs are need it will automatically be included in the dux4 table
    
    * style: split comment because linelength

--- workflow/scripts/export_to_xlsx_rna_fusions.py
+++ workflow/scripts/export_to_xlsx_rna_fusions.py
@@ -4,6 +4,7 @@ import xlsxwriter
 import datetime
 import sys
 import logging
+import operator
 
 logging.basicConfig(
     format="{asctime} - {levelname} - {message}",
@@ -28,8 +29,37 @@ def convert_columns_to_letter(nr_columns):
 
 """ Prepping input data """
 logging.info("Prepping input data")
+
+logging.info("Loading fusioncatcher genelist if exists")
+single_genes = set()
+gene_pairs = set()
+dux_pairs = set()
+
+if snakemake.params.fusioncatcher_genelist:
+    with open(snakemake.params.fusioncatcher_genelist, "r") as genelist_txt:
+        for lline in genelist_txt:
+            entry = lline.strip()
+            if not entry:
+                continue
+            if "|" in entry:
+                parts = [p.strip().upper() for p in entry.split("|") if p.strip()]
+                if len(parts) == 2:
+                    if "DUX4" in parts:
+                        dux_pairs.add(frozenset(parts))
+                    else:
+                        gene_pairs.add(frozenset(parts))
+                else:
+                    logging.error(f"Invalid fusion pair format: {entry}, only pairs allowed")
+                    sys.exit(1)
+            else:
+                single_genes.add(entry.upper())
+        logging.debug(f"Loaded {len(single_genes)=} single genes and {len(gene_pairs)=} gene pairs")
+        logging.debug(f"DUX4 genepairs: {dux_pairs=}")
+        logging.debug("Genes included in Fusioncatcher short table: " + str(single_genes | gene_pairs))
+
+
+logging.info(f"Loading arriba results: {snakemake.input.arriba=}")
 sample_name = snakemake.input.arriba.split("/")[-1].split("_")[0]
-logging.debug(f"Loading arriba results: {snakemake.input.arriba=}")
 arriba_table = {"headers": [], "data": []}
 with open(snakemake.input.arriba, "r") as arriba_tsv:
     for lline in arriba_tsv:
@@ -39,20 +69,41 @@ with open(snakemake.input.arriba, "r") as arriba_tsv:
             arriba_table["data"].append(lline.strip().split("\t"))
 
 
-logging.debug(f"Loading fusioncatcher results: {snakemake.input.fusioncatcher=}")
+# Loading fusioncatcher results in to three different lists/tables
+#     - fusioncatcher_table: the whole table
+#     - fusioncatcher_table_short: fusioncatcher results with only genes from single_genes and gene_pairs included,
+#           inframe fusions in one list and the rest in another to be able to print with in-frame fusions first
+#     - fusioncatcher_dux_table: all genepairs that include DUX4 from the genelist
+logging.info(f"Loading fusioncatcher results: {snakemake.input.fusioncatcher=}")
 fusioncatcher_table = {"headers": [], "data": []}
+fusioncatcher_table_short = {"inframe_rows": [], "the_rest_rows": []}
+fusioncatcher_dux_table = {"data": []}
 with open(snakemake.input.fusioncatcher, "r") as fusioncatcher_tsv:
     first_row = True
     for lline in fusioncatcher_tsv:
         line = lline.strip().split("\t")
         if first_row:
             [fusioncatcher_table["headers"].append({"header": column}) for column in line]
+            predicted_effect_idx = line.index("Predicted_effect")
             first_row = False
         else:
             fusioncatcher_table["data"].append(line)
+            g1 = line[0].strip().upper()
+            g2 = line[1].strip().upper()
+            predicted_effect = line[predicted_effect_idx].strip().lower()
+            if frozenset([g1, g2]) in dux_pairs:
+                fusioncatcher_dux_table["data"].append(line)
+            elif g1 in single_genes or g2 in single_genes or frozenset([g1, g2]) in gene_pairs:
+                if "in-frame" in predicted_effect:
+                    fusioncatcher_table_short["inframe_rows"].append(line)
+                else:
+                    fusioncatcher_table_short["the_rest_rows"].append(line)
+logging.debug(f"Number of dux4-pairs found: {len(fusioncatcher_dux_table['data'])=}")
+fusioncatcher_table_short["data"] = fusioncatcher_table_short["inframe_rows"] + fusioncatcher_table_short["the_rest_rows"]
+logging.debug(f"Number of fusions in Fusioncatcher short table: {len(fusioncatcher_table_short['data'])=}")
 
 
-logging.debug(f"Loading starfusion input: {snakemake.input.star_fusion=}")
+logging.info(f"Loading starfusion input: {snakemake.input.star_fusion=}")
 starfusion_table = {"headers": [], "data": []}
 with open(snakemake.input.star_fusion, "r") as starfusion_tsv:
     for lline in starfusion_tsv:
@@ -60,20 +111,6 @@ with open(snakemake.input.star_fusion, "r") as starfusion_tsv:
             [starfusion_table["headers"].append({"header": column}) for column in lline[1:].strip().split("\t")]
         else:
             starfusion_table["data"].append(lline.strip().split("\t"))
-
-
-logging.debug(f"Loading dux4 counts: {snakemake.input.dux4_igh_counts=}")
-with open(snakemake.input.dux4_igh_counts, "r") as counts_txt:
-    lline = counts_txt.readline()
-    dux_calls = lline.strip().split("\t")[0]
-
-logging.debug(f"Loading dux4 hits: {snakemake.input.dux4_igh_calls=}")
-dux4_table = {"headers": fusioncatcher_table["headers"], "data": []}
-with open(snakemake.input.dux4_igh_calls, "r") as calls_txt:
-    for lline in calls_txt:
-        if lline.strip() != "none":
-            line = lline.strip().split("\t")
-            dux4_table["data"].append(line)
 
 
 """ Creating xlsx file """
@@ -103,16 +140,38 @@ worksheet_overview.write_url(9, 0, "internal:'Fusioncatcher'!A1", string="Fusion
 worksheet_overview.write_url(10, 0, "internal:'StarFusion'!A1", string="StarFusion results")
 
 worksheet_overview.write(12, 0, "DUX4-IGH and DUX4-ERG hits from Fusioncatcher", format_bold)
-worksheet_overview.write(13, 0, "Number of hits: " + dux_calls)
+worksheet_overview.write(13, 0, "Number of hits: " + str(len(fusioncatcher_dux_table["data"])))
+worksheet_overview.write(
+    15,
+    0,
+    "Genes included in Fusioncatcher short table: "
+    + ", ".join(sorted(list(single_genes) + ["|".join(sorted(pair)) for pair in gene_pairs])),
+)
 
-i = 15
-column_end = convert_columns_to_letter(len(dux4_table["headers"]))
-if len(dux4_table["data"]) > 0:
-    table_area = "A" + str(i) + ":" + column_end + str(len(dux4_table["data"]) + i)
+i = 18
+logging.debug(f"Creating the DUX4 table")
+column_end = convert_columns_to_letter(len(fusioncatcher_table["headers"]))
+if len(fusioncatcher_dux_table["data"]) > 0:
+    table_area_dux = "A" + str(i) + ":" + column_end + str(len(fusioncatcher_dux_table["data"]) + i)
 else:
-    table_area = "A" + str(i) + ":" + column_end + str(i + 1)
+    table_area_dux = "A" + str(i) + ":" + column_end + str(i + 1)
+
 worksheet_overview.add_table(
-    table_area, {"columns": dux4_table["headers"], "data": dux4_table["data"], "style": "Table Style Light 1"}
+    table_area_dux,
+    {"columns": fusioncatcher_table["headers"], "data": fusioncatcher_dux_table["data"], "style": "Table Style Light 1"},
+)
+
+
+i = i + len(fusioncatcher_dux_table["data"]) + 3
+logging.debug(f"Creating fusioncatcher short table")
+if len(fusioncatcher_table_short["data"]) > 0:
+    table_area_short = "A" + str(i) + ":" + column_end + str(len(fusioncatcher_table_short["data"]) + i)
+else:
+    table_area_short = "A" + str(i) + ":" + column_end + str(i + 1)
+
+worksheet_overview.add_table(
+    table_area_short,
+    {"columns": fusioncatcher_table["headers"], "data": fusioncatcher_table_short["data"], "style": "Table Style Light 1"},
 )
 
 # Arriba sheet
